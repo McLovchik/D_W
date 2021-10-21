42cb2ad36141ec0154b978ea93d7e4a0ff28089c
McLovchik <aleksandrignatev@bk.ru>
2021-10-22 01:33:52 +0300

    timely-two

--- js/sliders.js
+++ js/sliders.js
@@ -1,48 +1,220 @@
-var swiper1 = new Swiper(".mySwiper1", {
-  pagination: {
-    el: ".swiper-pagination1",
-    type: "fraction",
-  },
-  grid: {
-    rows: 2,
-  },
-  loop: true,
-  navigation: {
-    nextEl: ".swiper-button-next1",
-    prevEl: ".swiper-button-prev1",
-  },
+
+let gallerySlider = new Swiper(".gallery__swiper-content", {
   slidesPerView: 3,
   slidesPerGroup: 3,
-});
-
-var swiper2 = new Swiper(".mySwiper2", {
-  pagination: {
-    el: ".swiper-pagination2",
-    type: "fraction",
-  },
   grid: {
-    rows: 1,
+    rows: 2
   },
-  loop: true,
+  spaceBetween: 30,
+  pagination: {
+    el: ".gallery__swiper-pagination",
+    type: "fraction"
+  },
   navigation: {
-    nextEl: ".swiper-button-next2",
-    prevEl: ".swiper-button-prev2",
+    nextEl: ".gallery__swiper-btn--next",
+    prevEl: ".gallery__swiper-btn--prev"
   },
-  slidesPerView: 3,
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      grid: {
+        rows: 1
+      },
+      spaceBetween: 0
+    },
+    576: {
+      slidesPerView: 2,
+      grid: {
+        rows: 2
+      },
+      spaceBetween: 30
+    },
+
+    1200: {
+      slidesPerView: 3,
+      grid: {
+        rows: 2
+      },
+      spaceBetween: 50
+    }
+  },
+
+  a11y: {
+    prevSlideMessage: 'Предыдущий',
+    nextSlideMessage: 'Следующий',
+  }
+
+  // on: {
+  //   /* исправляет баг с margin-top остающимся при смене брейкпоинта, это было нужно в 6-й версии свайпера */
+  //   beforeResize: function () {
+  //     this.slides.forEach((el) => {
+  //       el.style.marginTop = "";
+  //     });
+  //   }
+  // }
 });
 
-var swiper3 = new Swiper(".mySwiper3", {
+let editionsSlider = new Swiper(".editions__swiper-content", {
   slidesPerView: 3,
+  slidesPerGroup: 3,
+  grid: {
+    rows: 1
+  },
   spaceBetween: 30,
-  slidesPerGroup: 1,
-  loop: true,
-  loopFillGroupWithBlank: true,
   pagination: {
-    el: ".swiper-pagination3",
-    clickable: true,
+    el: ".editions__swiper-pagination",
+    type: "fraction"
   },
   navigation: {
-    nextEl: ".swiper-button-next3",
-    prevEl: ".swiper-button-prev3",
+    nextEl: ".editions__swiper-btn--next",
+    prevEl: ".editions__swiper-btn--prev"
   },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      grid: {
+        rows: 1
+      },
+      spaceBetween: 0
+    },
+    576: {
+      slidesPerView: 2,
+      grid: {
+        rows: 2
+      },
+      spaceBetween: 30
+    },
+
+    1200: {
+      slidesPerView: 3,
+      grid: {
+        rows: 1
+      },
+      spaceBetween: 50
+    }
+  },
+
+  a11y: {
+    prevSlideMessage: 'Предыдущий',
+    nextSlideMessage: 'Следующий',
+  }
+
+  // on: {
+  //   /* исправляет баг с margin-top остающимся при смене брейкпоинта, это было нужно в 6-й версии свайпера */
+  //   beforeResize: function () {
+  //     this.slides.forEach((el) => {
+  //       el.style.marginTop = "";
+  //     });
+  //   }
+  // }
 });
+
+// var swiper1 = new Swiper(".mySwiper1", {
+//   pagination: {
+//     el: ".swiper-pagination1",
+//     type: "fraction",
+//   },
+//   grid: {
+//     rows: 2,
+//   },
+//   loop: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next1",
+//     prevEl: ".swiper-button-prev1",
+//   },
+//   slidesPerView: 3,
+//   slidesPerGroup: 3,
+// });
+
+// var swiper2 = new Swiper(".mySwiper2", {
+//   pagination: {
+//     el: ".swiper-pagination2",
+//     type: "fraction",
+//   },
+//   grid: {
+//     rows: 1,
+//   },
+//   loop: true,
+//   navigation: {
+//     nextEl: ".swiper-button-next2",
+//     prevEl: ".swiper-button-prev2",
+//   },
+//   slidesPerView: 3,
+// });
+
+
+
+let projectsSlider = new Swiper(".projects__swiper-content", {
+  slidesPerView: 3,
+  slidesPerGroup: 3,
+  grid: {
+    rows: 1
+  },
+  spaceBetween: 30,
+  pagination: {
+    el: ".projects__swiper-pagination",
+    type: "fraction"
+  },
+  navigation: {
+    nextEl: ".projects__swiper-btn--next",
+    prevEl: ".projects__swiper-btn--prev"
+  },
+
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      grid: {
+        rows: 1
+      },
+      spaceBetween: 0
+    },
+    576: {
+      slidesPerView: 2,
+      grid: {
+        rows: 2
+      },
+      spaceBetween: 30
+    },
+
+    1200: {
+      slidesPerView: 3,
+      grid: {
+        rows: 1
+      },
+      spaceBetween: 50
+    }
+  },
+
+  a11y: {
+    prevSlideMessage: 'Предыдущий',
+    nextSlideMessage: 'Следующий',
+  }
+
+  // on: {
+  //   /* исправляет баг с margin-top остающимся при смене брейкпоинта, это было нужно в 6-й версии свайпера */
+  //   beforeResize: function () {
+  //     this.slides.forEach((el) => {
+  //       el.style.marginTop = "";
+  //     });
+  //   }
+  // }
+});
+
+
+// var swiper3 = new Swiper(".mySwiper3", {
+//   slidesPerView: 3,
+//   spaceBetween: 30,
+//   slidesPerGroup: 1,
+//   loop: true,
+//   loopFillGroupWithBlank: true,
+//   pagination: {
+//     el: ".swiper-pagination3",
+//     clickable: true,
+//   },
+//   navigation: {
+//     nextEl: ".swiper-button-next3",
+//     prevEl: ".swiper-button-prev3",
+//   },
+// });
