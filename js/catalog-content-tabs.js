c09fafd820f672c1a38680f99fcf3d91ca5feb38
McLovchik <aleksandrignatev@bk.ru>
2021-10-22 15:39:09 +0300

    before changes in first blocks

---
+++ js/catalog-content-tabs.js
@@ -0,0 +1,11 @@
+$( function() {
+    $( "#catalog__accordion-italy" ).accordion({
+      collapsible: true,
+    });
+});
+
+$( function() {
+    $( "#catalog__accordion-germany" ).accordion({
+      collapsible: true,
+    }); 
+});
